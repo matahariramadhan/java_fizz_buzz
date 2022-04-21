0d40cc5ecf65e8cf96979694ab12270086172307
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-04-21 21:04:40 +0200

    adding repeat functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isRepeat = true;
+        while(isRepeat == true){
+            System.out.print("Number: ");
+            byte number = scanner.nextByte();
+            if (number % 5 == 0 && number % 3 == 0){
+                System.out.println("FizzBuzz");
+            }else if (number % 3 == 0 ){
+                System.out.println("Buzz");
+            } else if (number % 5 == 0) {
+                System.out.println("Fizz");
+            } else{
+                System.out.println(number);
+            }
+
+            boolean isRepeatValidated = false;
+            while(isRepeatValidated == false){
+                System.out.println("Again (y/n): ");
+                String input = scanner.next();
+                switch (input){
+                    case "y":
+                        isRepeat = true;
+                        isRepeatValidated = true;
+                        break;
+                    case  "n":
+                        isRepeat = false;
+                        isRepeatValidated = true;
+                        break;
+                    default:
+                        System.out.println("Please input \"y\" for yes or \"n\" for no");
+                        isRepeatValidated = false;
+            }
+
+            }
+        }
     }
 }
